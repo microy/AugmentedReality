78cd43bf8c6f698338e118221cfe14612782888b
Michaël Roy <microygh@gmail.com>
2016-09-08 13:47:48 +0200

    Add a script to setup the USB camera.

---
+++ disable-autofocus.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+#
+# Configure the USB camera
+#
+
+# Disable autofocus
+uvcdynctrl -v -d video0 --set='Focus, Auto' 0
+
+# Fix the focus
+uvcdynctrl -v -d video0 --set='Focus (absolute)' 30
+
+# Fix power line frequency
+uvcdynctrl -v -d video0 --set='Power Line Frequency' 1
