0599137dd13863766055c643d8f036a2a910a749
Michaël Roy <microygh@gmail.com>
2016-09-09 16:50:11 +0200

    Add chessboard detection.

--- test.py
+++ test.py
@@ -7,6 +7,10 @@
 
 # External dependencies
 import cv2
+import numpy as np
+
+# Calibration pattern size
+pattern_size = ( 9, 6 )
 
 # Main application
 if __name__ == '__main__' :
@@ -14,9 +18,15 @@ if __name__ == '__main__' :
     while( True ) :
         # Capture frame-by-frame
         _, frame = cap.read()
+        # Find the chessboard corners on the image
+        found, corners = cv2.findChessboardCorners( frame, pattern_size, flags = cv2.CALIB_CB_FAST_CHECK )
+        # Draw the chessboard corners on the image
+        if found :
+            cv2.drawChessboardCorners( frame, pattern_size, corners, found )
+        #    cv2.rectangle( frame, (int(corners[0,0,0]), int(corners[0,0,1])), (int(corners[53,0,0]), int(corners[53,0,1])), (0,255,0), 3 )
         # Display the resulting frame
         cv2.imshow( 'frame', frame )
+        # Exit application with the 'q' key
         if cv2.waitKey( 1 ) & 0xFF == ord( 'q' ) : break
-    # When everything done, release the capture
-    cap.release()
-#    cv2.destroyAllWindows()
+    # Close the windows
+    cv2.destroyAllWindows()
