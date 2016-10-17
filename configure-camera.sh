bfbb1551fb9ae754b81ee1e0bf47f0d3566e17fd
Michaël Roy <microygh@gmail.com>
2016-10-17 11:57:22 +0200

    Disable autofocus.

--- configure-camera.sh
+++ configure-camera.sh
@@ -6,3 +6,9 @@
 
 # Fix power line frequency
 uvcdynctrl -v -d video0 --set='Power Line Frequency' 1
+
+# Disable autofocus
+uvcdynctrl -v -d video0 --set='Focus, Auto' 0
+
+# Fix the focus
+uvcdynctrl -v -d video0 --set='Focus (absolute)' 0
